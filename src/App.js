63f228dd1dcd588fb1b13f907e11285a8d2d4b8a
ozantalay <ozntalay@hotmail.com>
2024-10-07 19:22:02 +0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 const BASE_IMG_URL = "https://picsum.photos/seed/sameimage/300";
 
@@ -7,7 +7,34 @@ function App() {
 }
 
 const CustomBlur = () => {
-  // KODUNUZ BURAYA GELECEK
+  const [blurValue, setBlurValue] = useState(0);
+  const imgStyle = {
+    filter: `blur(${blurValue}px)`,
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <img
+        style={imgStyle}
+        src={`${BASE_IMG_URL}`}
+        alt="Doğa Resmi"
+        className="rounded-lg shadow-lg transition duration-300 ease-in-out"
+        width="300"
+        height="200"
+      />
+      <input
+        type="range"
+        min="0"
+        max="10"
+        value={blurValue}
+        onChange={(e) => setBlurValue(e.target.value)}
+        className="mt-4 bg-gray-300 rounded-lg appearance-none cursor-pointer"
+      />
+      <div className="text-center mt-2">
+        <span className="text-lg font-semibold">Bulanıklık: {blurValue}px</span>
+      </div>
+    </div>
+  );
 };
 
 export default App;
