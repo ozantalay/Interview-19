6f5821ad190c60ed801e5e88d300fcb2b9df4e4b
ozantalay <ozntalay@hotmail.com>
2024-10-07 19:01:53 +0300

    baslangic

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+
+const BASE_IMG_URL = "https://picsum.photos/seed/sameimage/300";
+
+function App() {
+  return <CustomBlur />;
+}
+
+const CustomBlur = () => {
+  // KODUNUZ BURAYA GELECEK
+};
+
+export default App;
